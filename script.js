53044cecc84af5a51fdf77324c3c58bee150369c
andyleemk <57303508+andyleemk@users.noreply.github.com>
2020-05-10 12:00:23 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var TxtRotate = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
     this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
+    this.period = parseInt(period, 9) || 2000;
     this.txt = '';
     this.tick();
     this.isDeleting = false;
@@ -23,7 +23,7 @@ var TxtRotate = function(el, toRotate, period) {
     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
   
     var that = this;
-    var delta = 300 - Math.random() * 100;
+    var delta = 160 - Math.random() * 100;
   
     if (this.isDeleting) { delta /= 2; }
   
